869238428bdfb445d028e8aa854caa1cadb7ae78
Parkavan2003 <parkxkumar1805@gmail.com>
2025-11-21 21:00:47 +0530

    production

--- utils/index.js
+++ utils/index.js
@@ -7,7 +7,7 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure:false , // Use secure cookies in production process.env.NODE_ENV !== "development"
+    secure: process.env.NODE_ENV !== "development" , // Use secure cookies in production process.env.NODE_ENV !== "development"
     sameSite: "None", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
